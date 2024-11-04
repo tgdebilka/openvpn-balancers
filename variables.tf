21662489debf98b8efa0b6483a283a9126fb1eec
Anastasia <113984039+tgdebilka@users.noreply.github.com>
2024-11-04 20:12:27 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "image_name" {
+  type = string
+  default = "ubuntu-20-202404160933.gitd6495fe9"
+}
+
+variable "compute_flavor" {
+  type = string
+  default = "STD2-1-1"
+}
+
+variable "key_pair_name" {
+  type = string
+  default = "***"
+}
+
+variable "availability_zone_name" {
+  type = string
+  default = "MS1"
+}
+
+variable "availability_zone_name2" {
+  type = string
+  default = "GZ1"
+}
+
+variable "availability_zone_name3" {
+  type = string
+  default = "ME1"
+}
